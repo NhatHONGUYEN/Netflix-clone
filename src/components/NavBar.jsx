43c4298d9e371fe4ec4c6cef28f30b5fc6f611ee
NhatHONGUYEN <hnnhat67@gmail.com>
2024-05-16 02:25:43 +0200

    ✨    F02 : Pages > Add Login and SignUp

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function NavBar() {
   return (
     <div className="flex w-full justify-between items-center p-4 z-[100] absolute align-center">
-      <h1 className="text-red-600 text-xl font-bold cursor-pointer ">
-        NETFLIX
-      </h1>
+      <Link to="/">
+        <h1 className="text-red-600 text-xl font-bold cursor-pointer ">
+          NETFLIX
+        </h1>
+      </Link>
       <div className="text-xs">
-        <button className="text-white pr-4  ">Sign In</button>
-        <button className="text-white px-4 py-2 cursor-pointer  bg-red-600  rounded ">
-          Sign Up
-        </button>
+        <Link to="/login">
+          <button className="text-white pr-4  ">Sign In</button>
+        </Link>
+        <Link to="/signup">
+          <button className="text-white px-4 py-2 cursor-pointer  bg-red-600  rounded ">
+            Sign Up
+          </button>
+        </Link>
       </div>
     </div>
   );
--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,3 +1,53 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="w-full h-screen">
+      <img
+        className="hidden sm:block absolute w-full h-full object-cover"
+        src="/netflix image.jpg"
+        alt="netflix_image"
+      />
+      <div className="bg-black/60 w-full h-full fixed top-0 left-0"></div>
+      <div className="fixed w-full px-4 py-24 z-50">
+        <div className="rounded max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+          <div className="max-w-[320px] mx-auto py-16">
+            <div className="text-3xl font-bold  ">Sign In</div>
+            <form className="w-full flex flex-col py-4">
+              <input
+                type="email"
+                placeholder="Email"
+                autoComplete="email"
+                className="bg-gray-700 p-3 my-2 rounded"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                autoComplete="current-password"
+                className="bg-gray-700 p-3 my-2 rounded"
+              />
+              <button className="bg-red-600 text-white py-3 my-6 font-bold rounded-md">
+                Sign In
+              </button>
+              <div className="flex justify-between items-center text-gray-600">
+                <p>
+                  <input className="mr-2" type="checkbox" />
+                  Remember me
+                </p>
+                <p>Need Help?</p>
+              </div>
+
+              <p className="py-8">
+                <span className="text-gray-600 mr-2">
+                  Already subscribed to Netflix?
+                </span>
+
+                <Link to="/login">Sign In</Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
