8ae6841c6afe55b55ced51a5f970ca38cec66d28
NhatHONGUYEN <hnnhat67@gmail.com>
2024-05-17 10:51:26 +0200

    ✨     F02 : Add the delete function to our saveshow

--- src/page/Account.jsx
+++ src/page/Account.jsx
@@ -1,3 +1,19 @@
+import SavedShow from "../components/SavedShow.jsx";
+
 export default function Account() {
-  return <div>Account</div>;
+  return (
+    <div className="w-full h-[550px] ">
+      <div className="absolute w-full h-[550px] bg-black/60 "></div>
+      <img
+        className="w-full h-[550px] object-cover"
+        src="/netflix image.jpg
+  "
+        alt="netflixImage"
+      />
+      <div className="absolute top-40 left-4 text-white text-3xl md:text-5xl font-bold">
+        My Show
+      </div>
+      <SavedShow />
+    </div>
+  );
 }
